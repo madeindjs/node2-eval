d91c0fa4a2390f6eefbe39d515bad0709ecbaa4d
Antoine Peyrin <antoine.peyrin@hotmail.fr>
2018-11-15 09:34:56 +0100

    add base docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+cd /home/node/projet/
+
+##
+## Run express (you need nodemon if you want it to work nodemon -L)
+##
+echo "--------------"
+echo "Launch App"
+echo "--------------"
+npm run dev
