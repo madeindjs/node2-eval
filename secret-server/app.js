e692007aabf87506477cc11a454d9aa8847c39c3
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2018-11-15 15:53:45 +0100

    Littles corrections

--- secret-server/app.js
+++ secret-server/app.js
@@ -7,6 +7,13 @@ const secretPath = path.join(__dirname, 'data', 'secret.txt')
 const app = express()
 const port = 3000
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 
 function encrypt(buffer) {
   return Buffer.from(buffer).toString('base64')
